28011b6fc1286f82660ea20fe4d71446f6a8afcf
Ryan Ming <ryan.ming@gmail.com>
2014-10-15 19:20:38 -0400

    added object exercises

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.apply(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+      result = [];
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          result.push(key + ': ' + obj[key]);
+        }
+      }
+      return result;
     }
   };
 });

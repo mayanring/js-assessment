7618f6d6cb5e4218dfbffc5538b8f2869fc35411
Ryan Ming <ryan.ming@gmail.com>
2014-10-15 21:08:27 -0400

    added regex exercises

--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,30 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        return str.search(/\d+/) !== -1;
     },
 
     containsRepeatingLetter : function(str) {
-
+        return str.search(/([A-Za-z])\1+/) !== -1;
     },
 
     endsWithVowel : function(str) {
-
+        return str.search(/[aeiouAEIOU]$/) !== -1;
     },
 
     captureThreeNumbers : function(str) {
+        var result = str.match(/(\d{3})/);
+        if (result && result[1]) {
+            return result[1];
+        }
 
+        return false;
     },
 
     matchesPattern : function(str) {
-
+        return str.search(/^\d{3}-\d{3}-\d{4}$/) !== -1;
     },
     isUSD : function(str) {
-
     }
   };
 });
